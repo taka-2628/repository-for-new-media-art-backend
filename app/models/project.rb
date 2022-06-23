fd2ab08b1fe830b68da70b7d344480e24ea774b8
Takashi <th2826@columbia.edu>
2022-06-22 23:35:45 -0400

    associations between models created

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,7 @@
 class Project < ActiveRecord::Base
+  belongs_to :user
+  
+  has_many :comments
+  has_many :genres
+  has_many :technologies
 end 
--- app/models/technology.rb
+++ app/models/technology.rb
@@ -1,2 +1,3 @@
 class Technology < ActiveRecord::Base
+  belongs_to :project
 end 
